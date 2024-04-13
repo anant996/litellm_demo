c16c3fcf924722c7e6c5b9e151e1e0dba04416b1
anant996 <anant.chaudhary@dataverze.ai>
2024-04-13 16:46:51 +0530

    added azure openai model

--- multi_model_completion.py
+++ multi_model_completion.py
@@ -1,5 +1,8 @@
 from litellm import completion
 import boto3  
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 messages = [{ "content": "What are the 5 key features of jenkins?","role": "user"}]
 
@@ -13,3 +16,8 @@ client = boto3.client("bedrock-runtime", region_name="us-east-1")
 response = completion(model="bedrock/anthropic.claude-v2:1",messages = messages,aws_bedrock_client=client)
 print(response)
 print("Claude-V2:1: ", response['choices'][0]['message']['content'])
+
+# azure openai
+response = completion(model = "azure/lai-gpt-35-turbo", messages = messages)
+print(response)
+print("Azure OPENAI: ", response['choices'][0]['message']['content'])
